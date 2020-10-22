274bf03f913de93b8835e33be84d712e4cde7ed6
Jack <29169102+Jack5079@users.noreply.github.com>
2020-10-22 18:54:12 -0400

    aaaa

--- search.js
+++ search.js
@@ -25,9 +25,28 @@ async function getDir() {
   return fetch(`https://api.github.com/repos/9701ml/9701ml.github.io/git/trees/${sha}?recursive=1`).then(res => res.json())
 };
 
+/**
+ * @param {github} gh
+ */
+function populateSearch({ tree }) {
+  tree.forEach(async ele => {
+    const text = await fetch('/tools/' + ele.path).then(res => res.text())
+    const doc = new DOMParser().parseFromString(text, 'text/html')
+    const result = document.createElement('article')
+    const link = document.createElement('a')
+    link.innerText = doc.querySelector('title').innerText || 'Name not found'
+    link.href = '/tools/' + ele.path
+    const description = document.createElement('p')
+    description.innerText = doc.querySelector('meta[name="description"]').getAttribute('content') || 'Description not found'
+    result.append(link, description)
+    document.body.append(result)
+  })
+};
+
 (async () => {
-  const { tree } = await getDir()
-  if (!search) {
-    console.log(tree)
-  }
-})().catch(console.error)
+  const gh = await getDir()
+  populateSearch(gh)
+})().catch(() => {
+  // @ts-ignore
+  document.querySelector('#error').style.display = 'block'
+})

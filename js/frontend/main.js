f67448e9c416003be68033f06b3527b666e63266
Jack <git@5079.ml>
2021-07-23 14:41:55 -0400

    v2 is starting

---
+++ js/frontend/main.js
@@ -0,0 +1,36 @@
+import options from './options.js'
+
+const input = document.querySelector('input')
+const main = document.querySelector('main')
+
+/**
+ * @param {string} uri
+ * @param {string} name
+ */
+function downloadURI(uri, name) {
+  const link = document.createElement("a")
+  document.body.append(link)
+  link.download = name
+  link.href = uri
+  link.click()
+  link.remove()
+}
+
+/**
+ * @param {File} file
+ * @returns {import('./types').Option[]}
+ */
+const get_options = file => options.filter(option => Array.isArray(option.type) ? option.type.some(type => file.type.startsWith(type)) : file.type.startsWith(option.type))
+
+input.addEventListener('change', () => {
+  const file = input.files.item(0)
+  for (const option of get_options(file)) {
+    const btn = document.createElement('button')
+    btn.textContent = option.name
+    btn.addEventListener('click', async () => {
+      const {file:blob,name} = await option.run(file)
+      downloadURI(URL.createObjectURL(blob), name)
+    })
+    main.append(btn)
+  }
+})
